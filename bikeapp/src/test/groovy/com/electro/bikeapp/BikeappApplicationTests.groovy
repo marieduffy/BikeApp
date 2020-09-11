6cfa59f619fe93c6b9a8c91f3a386932e30dc486
Jacob Byerline <jbyerline@gmail.com>
2020-09-10 21:06:38 -0700

    Added springboot

---
+++ bikeapp/src/test/groovy/com/electro/bikeapp/BikeappApplicationTests.groovy
@@ -0,0 +1,13 @@
+package com.electro.bikeapp
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BikeappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

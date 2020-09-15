5a4481a585b1abe46ca86faca1f197cf01308b60
Jacob Byerline <jbyerline@gmail.com>
2020-09-14 21:32:31 -0700

    added utils

---
+++ bikeapp/src/main/groovy/utils/OffsetDateTimePersistenceConverter.groovy
@@ -0,0 +1,50 @@
+package utils
+
+import groovy.util.logging.Slf4j
+
+import javax.persistence.AttributeConverter
+import javax.persistence.Converter
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+
+/**
+ * Created by sk48 on 11/14/2019.
+ * This class is used to convert the offsetdatetime to include proper offset during persistance
+ * instead of +00
+ * Reference: https://hibernate.atlassian.net/browse/HHH-10235 and
+ * https://stackoverflow.com/questions/35808423/
+ * hibernate-not-correctly-storing-zoneddatetime-as-datetimeoffset-in-sql-server
+ */
+@Slf4j
+@Converter
+class OffsetDateTimePersistenceConverter implements AttributeConverter<OffsetDateTime, String> {
+
+    private static final DateTimeFormatter FORMATTER_FROM_TO_DB = DateTimeFormatter.ofPattern(
+            'yyyy-MM-dd HH:mm:ss')
+
+    /**
+     * This method converts date in a given format to a database understandable format
+     * @return a value as a String such as "2014-12-03 10:15:30.9870000 +01:00"
+     * @see java.time.OffsetDateTime#toString()
+     */
+    @Override
+    String convertToDatabaseColumn(OffsetDateTime entityValue) {
+        if (entityValue == null) {
+            return null
+        }
+        entityValue.format(FORMATTER_FROM_TO_DB)
+    }
+
+    /**
+     * This method reads date as string from database and converts it to date in given format
+     * @return a value as a OffsetDateTime such as 2014-12-03T10:15:30.9870000+01:00
+     */
+    @Override
+    OffsetDateTime convertToEntityAttribute(String databaseValue) {
+        if (databaseValue == null) {
+            return null
+        }
+        log.info("Date returned from DB: $databaseValue")
+        OffsetDateTime.parse(databaseValue, FORMATTER_FROM_TO_DB)
+    }
+}
---
+++ bikeapp/src/main/groovy/utils/UtcDateSerializer.groovy
@@ -0,0 +1,28 @@
+package utils
+
+import com.fasterxml.jackson.core.JsonGenerator
+import com.fasterxml.jackson.databind.JsonSerializer
+import com.fasterxml.jackson.databind.SerializerProvider
+
+import java.time.OffsetDateTime
+import java.time.format.DateTimeFormatter
+
+/**
+ * Created by sk48 on 11/14/2019.
+ * This class is used to convert the datetimes in given timezone with offset to UTC.
+ * This also serializes the given offsetdatetime to a JSON format.
+ * Given a DateTimeOffset, format it as an ISO_INSTANT in UTC
+ * Example: input is 2019-04-01 01:13:06.121 +02:00, output
+ * 2019-03-30T23:13:06.121Z
+ */
+class UtcDateSerializer extends JsonSerializer<OffsetDateTime> {
+    @Override
+    void serialize(OffsetDateTime givenOffsetDateTime, JsonGenerator gen, SerializerProvider serializers)
+            throws IOException {
+        if (givenOffsetDateTime) {
+            String formattedDateTime = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSX").format(
+                    givenOffsetDateTime.atZoneSameInstant(java.time.ZoneId.of('Z')))
+            gen.writeString(formattedDateTime)
+        }
+    }
+}

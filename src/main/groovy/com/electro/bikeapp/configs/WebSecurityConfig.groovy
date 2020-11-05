d0bb1fe4b8a39e4260c8a8830fb1f6a3c452ac5e
Jacob Byerline <jbyerline@gmail.com>
2020-11-04 20:26:19 -0800

    Finished Encryption

--- src/main/groovy/com/electro/bikeapp/configs/WebSecurityConfig.groovy
+++ src/main/groovy/com/electro/bikeapp/configs/WebSecurityConfig.groovy
@@ -4,34 +4,39 @@ import com.electro.bikeapp.utils.StringEncryption
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.core.userdetails.UserDetailsService
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher
 
 @Configuration
 @EnableWebSecurity
 class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    UserDetailsService userDetailsService;
+    UserDetailsService userDetailsService
 
     @Bean
-    public DaoAuthenticationProvider authProvider() {
-        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
-        authProvider.setUserDetailsService(userDetailsService());
-        authProvider.setPasswordEncoder(new StringEncryption());
-        return authProvider;
+    PasswordEncoder encoder() {
+        return new StringEncryption()
+    }
+
+    @Bean
+    DaoAuthenticationProvider authProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider()
+        authProvider.setUserDetailsService(userDetailsService())
+        authProvider.setPasswordEncoder(new StringEncryption())
+        return authProvider
     }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService);
+        auth.userDetailsService(userDetailsService)
     }
 
     @Override
--- src/main/groovy/com/electro/bikeapp/utils/StringEncryption.groovy
+++ src/main/groovy/com/electro/bikeapp/utils/StringEncryption.groovy
@@ -8,6 +8,8 @@ class StringEncryption implements PasswordEncoder{
 
     @Override
     String encode(CharSequence inputString) {
+
+        //log.info(inputString.toString())
         int inputStringLen
         int sqrtoflen
         int twoDimensionalArraySize
@@ -26,13 +28,13 @@ class StringEncryption implements PasswordEncoder{
         else twoDimensionalArraySize = sqrtoflen + 1
 
         // Make string length a perfect square by appending "_" to the end
-        for (int i = 0; i < twoDimensionalArraySize * 2; i++)
+        for (int i = 0; i < twoDimensionalArraySize * twoDimensionalArraySize - inputStringLen; i++)
         {
             inputString += "_"
         }
 
         // Initialize the 1D arrays, copy string into 1D tempArr
-        char[] tempArr = inputString.toCharArray()
+        char[] tempArr = inputString
         char[] OneDimensionalArray = new char[twoDimensionalArraySize * twoDimensionalArraySize]
 
         // Replace (space) with _
@@ -79,27 +81,57 @@ class StringEncryption implements PasswordEncoder{
             }
         }
 
-        // If string contains _, its been encrypted. So remove the _
-        if (inputString.contains("_"))
-        {
-            // Replace _ with (space)
-            for (int i = 0; i < OneDimensionalArray.length; i++)
-            {
-                if (OneDimensionalArray[i] == '_' as char)
-                {
-                    OneDimensionalArray[i] = ' '
-                }
-                //log.info(OneDimensionalArray[i] as String)
-            }
-        }
-
         // Concatenates the 1D array back into a string
         String answer = new String(OneDimensionalArray)
 
+        // Answer has been encrypted by one stage at this point
+
+        // Cast string to char array
+        char[] rot13 = answer.toCharArray()
+        char[] asciiArray = new char[answer.length()]
+
+        // Convert char array to array of ascii values
+        for(int i = 0; i < rot13.length; i++){
+            asciiArray[i] = (int)rot13[i]
+        }
+
+        // Shift each ascii value by 13 (symmetrical)
+        for(int i = 0; i < rot13.length; i++){
+            // Char a .. z
+            if(asciiArray[i] > 96 && asciiArray[i] < 123){
+                // Add 13
+                asciiArray[i] = asciiArray[i] + 13
+                // After we added, if you are no longer between a and z
+                if(asciiArray[i] > 122){
+                    // Wrap char back around to a
+                    asciiArray[i] = asciiArray[i] - 26
+                }
+            }
+            // Char A .. Z
+            else if(asciiArray[i] > 64 && asciiArray[i] < 91){
+                // Add 13
+                asciiArray[i] = asciiArray[i] + 13
+                // After we added, if you are no longer between A and Z
+                if(asciiArray[i] > 90){
+                    // Wrap char back around to A
+                    asciiArray[i] = asciiArray[i] - 26
+                }
+            }
+        }
+        // Make new string array
+        char[] stringArray = new char[answer.length()]
+        // Convert ascii values back to character values
+        for(int i = 0; i < rot13.length; i++){
+            stringArray[i] = (char)asciiArray[i]
+        }
+        // Convert char array to string
+        String fullyEncryptedPass = stringArray.toString()
 
         // Prints out encrypted string
-        //log.info(answer)
-        return answer
+        //log.info(fullyEncryptedPass)
+
+        // Return twice encrypted string
+        return fullyEncryptedPass
     }
 
     @Override
@@ -107,6 +139,10 @@ class StringEncryption implements PasswordEncoder{
         boolean matches = false
 
         String checkPassword = encode(rawPassword)
+
+        //log.info("RAW: " + rawPassword.toString())
+        //log.info("ENCODED: " + encodedPassword)
+        //log.info("CHECK: " + checkPassword)
 
         if(checkPassword == encodedPassword){
             matches = true

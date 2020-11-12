305c6e16ac417159828eed1d96bd5c8ecefe5a99
bathong188 <lebathong188@github.com>
2020-11-11 19:43:14 -0800

    ezmoney
    fixed SQL syntax for inventory repo. it doesn't like "=="

--- src/main/groovy/com/electro/bikeapp/repositories/InventoryRepository.groovy
+++ src/main/groovy/com/electro/bikeapp/repositories/InventoryRepository.groovy
@@ -16,12 +16,13 @@ interface InventoryRepository extends JpaRepository<BikeDomain, Long> {
 
     // SQL Query to say, if a JSON parameter is NULL, ignore it, otherwise, return the items from the database
     // that meet the given parameters. Normally, you won't need to hardcode a query like this
-    @Query("SELECT b FROM BikeDomain b WHERE (:bikeColor is null or b.color == :bikeColor) and (:priceMin is null or b.retailCost >= :priceMin) and (:priceMax is null or b.retailCost <= :priceMax) and (:inStock is null or b.inStock == :inStock) and (:condition is null or b.condition == :condition)")
+    @Query("SELECT b FROM BikeDomain b WHERE (:bikeColor is null or b.color = :bikeColor) and (:priceMin is null or b.retailCost >= :priceMin) and (:priceMax is null or b.retailCost <= :priceMax) and (:inStock is null or b.inStock = :inStock) and (:condition is null or b.condition = :condition) and (:make is null or b.make = :make)")
     // This method returns a list of bikes. It accepts the name, make, color, lessThanCost, and greaterThanCost
     List<BikeDomain> allParamSearch(@Param("bikeColor") String bikeColor,
                                     @Param("priceMin") Float priceMin,
                                     @Param("priceMax") Float priceMax,
                                     @Param("inStock") Boolean inStock,
-                                    @Param("condition") String condition);
+                                    @Param("condition") String condition,
+                                    @Param("make") String make);
 
 }

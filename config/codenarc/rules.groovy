dc258ec6427c608bb04a198d93e81679a4460144
Jacob Byerline <jbyerline@gmail.com>
2020-11-16 22:41:16 -0800

    Added CodeNarc

---
+++ config/codenarc/rules.groovy
@@ -0,0 +1,24 @@
+// RULE DEFINITIONS:
+// https://guides.grails.org/grails-codenarc/guide/index.html
+
+
+ruleset {
+    description 'Grails-CodeNarc Project RuleSet'
+
+    ruleset('rulesets/basic.xml')
+    ruleset('rulesets/braces.xml')
+    ruleset('rulesets/convention.xml'){
+        'CompileStatic' {
+            enabled = false
+        }
+    }
+    ruleset('rulesets/design.xml')
+    ruleset('rulesets/dry.xml')
+    ruleset('rulesets/exceptions.xml')
+    ruleset('rulesets/formatting.xml')
+    ruleset('rulesets/generic.xml')
+    ruleset('rulesets/imports.xml')
+    ruleset('rulesets/naming.xml')
+    ruleset('rulesets/unnecessary.xml')
+    ruleset('rulesets/unused.xml')
+}

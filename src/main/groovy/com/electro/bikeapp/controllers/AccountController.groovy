790ff39cc7f26bf6eab4a622bbdc6a592b58dd27
Jacob Byerline <jbyerline@gmail.com>
2020-11-30 19:39:37 -0800

    topnav.css

--- src/main/groovy/com/electro/bikeapp/controllers/AccountController.groovy
+++ src/main/groovy/com/electro/bikeapp/controllers/AccountController.groovy
@@ -121,7 +121,7 @@ class AccountController {
     @PostMapping(value = '/account/getPrivilege', produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.OK)
     String getPrivilege (@RequestBody String username) {
-        log.info(username)
+        log.info(username + " just logged in.")
         accountService.getPrivilege(username)
     }
 
